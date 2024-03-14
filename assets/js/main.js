87d50687e81c3eff36c46898f6b198ee83c38445
ifahimkhan <khan.fahim95@hotmail.com>
2024-03-14 23:01:27 +0530

    change theme for light mode and dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,7 +98,7 @@ window.addEventListener('scroll', scrollUp)
 /*=============== DARK LIGHT THEME ===============*/ 
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
-const iconTheme = 'ri-sun-line'
+const iconTheme = 'ri-lightbulb-line'
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme')
@@ -106,7 +106,7 @@ const selectedIcon = localStorage.getItem('selected-icon')
 
 // We obtain the current theme that the interface has by validating the dark-theme class
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-lightbulb-fill' : 'ri-lightbulb-line'
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
